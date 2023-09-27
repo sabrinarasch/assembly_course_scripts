96b1e843f7680212f17b8be2094143d1038d29dc
smira_student_account <sabrina.rasch@student.unibe.ch>
2023-09-27 14:16:24 +0200

    update flye and canu

--- 03_01_flye_assembly.sh
+++ 03_01_flye_assembly.sh
@@ -8,24 +8,19 @@
 #SBATCH --mail-type=begin,end,fail
 #SBATCH --output=/data/users/srasch/assembly_course/Output/output_flye_assembly_%j.o
 #SBATCH --error=/data/users/srasch/assembly_course/Error/error_flye_assembly_%j.e
-#SBATCH --partition=pcourseassembly
+#SBATCH --partition=pall
 
 #Add the modules
-    module add Flye/2.9-GCC-10.3.0
+    module add UHTS/Assembler/flye/2.8.3
 
 #Create directories and variables
     course_dir=/data/users/srasch/assembly_course
-    data_types=("Illumina" "pacbio" "RNAseq")    
     raw_data_dir=${course_dir}/RawData
 
     assembly_dir=${course_dir}/03_assembly
         mkdir ${assembly_dir}
-
-#Create the output directories
-    # for data_type in "${data_types[@]}"
-    # do
-    #     assembly_data_dir=${assembly_dir}/${data_type}
-    #         mkdir ${assembly_data_dir}
-    # done
+    flye_dir=${assembly_dir}/flye
+        mkdir ${flye_dir}
 
 #Do the asembly
+    flye --pacbio-raw ${raw_data_dir}/pacbio/*.fastq.gz -o ${flye_dir} -g 125m -t 8
--- 03_02_canu_assembly.sh
+++ 03_02_canu_assembly.sh
@@ -8,24 +8,23 @@
 #SBATCH --mail-type=begin,end,fail
 #SBATCH --output=/data/users/srasch/assembly_course/Output/output_canu_assembly_%j.o
 #SBATCH --error=/data/users/srasch/assembly_course/Error/error_canu_assembly_%j.e
-#SBATCH --partition=pcourseassembly
+#SBATCH --partition=pall
 
 #Add the modules
     module add UHTS/Assembler/canu/2.1.1
 
 #Create directories and variables
     course_dir=/data/users/srasch/assembly_course
-    data_types=("Illumina" "pacbio" "RNAseq")    
     raw_data_dir=${course_dir}/RawData
 
     assembly_dir=${course_dir}/03_assembly
         mkdir ${assembly_dir}
-
-#Create the output directories
-    # for data_type in "${data_types[@]}"
-    # do
-    #     assembly_data_dir=${assembly_dir}/${data_type}
-    #         mkdir ${assembly_data_dir}
-    # done
+    canu_dir=${assembly_dir}/canu
+        mkdir ${canu_dir}
 
 #Do the asembly
+    canu -p canu -d ${canu_dir} genomeSize=125m \
+    maxThreads=16 maxMemory=64 \
+    gridEngineResourceOption="--cpus-per-task=THREADS --mem-per-cpu=MEMORY" \
+    gridOptions="--partition=pall --mail-user=sabrina.rasch@students.unibe.ch" \
+    -pacbio ${raw_data_dir}/pacbio/*.fastq.gz
