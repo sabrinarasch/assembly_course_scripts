2e1e648df0aab8bfba85aa594134402ac9221529
smira_student_account <sabrina.rasch@student.unibe.ch>
2023-10-05 16:02:50 +0200

    Work out busco run

--- 03_03_BUSCO_evaluation.sh
+++ 03_03_BUSCO_evaluation.sh
@@ -30,17 +30,25 @@
     mkdir ${assembly_BUSCO_dir}
 
 #Specify the assembly to use (!!!COMMENT OUT THE ONE YOU ARE NOT USING!!!)
-    assembly=${polish_evaluation_dir}/polish/pilon/canu/***
-    # assembly=${polish_evaluation_dir}/polish/pilon/flye/***
+    assembly=${polish_evaluation_dir}/polish/pilon/canu/canu.fasta
+    # assembly=${polish_evaluation_dir}/polish/pilon/flye/flye.fasta
     # assembly=${course_dir}/02_assembly/trinity/Trinity.fasta
 
-#Run busco to ...
+#Go to folder where the evaluation results will be stored
+    cd ${assembly_BUSCO_dir}
+
+#Make a copy of the augustus config directory to a location where I have write permission
+    cp -r /software/SequenceAnalysis/GenePrediction/augustus/3.3.3.1/config augustus_config
+    export AUGUSTUS_CONFIG_PATH=./augustus_config
+
+#Run busco to assess the quality of the assemblies
     busco -i ${assembly} -l brassicales_odb10 -o ${assembly_name} -m genome --cpu 8
         #Options entered here are:
             #"-i": Input sequence file in FASTA format. Can be an assembled genome or transcriptome (DNA), or protein sequences from an annotated gene set.
             #"-l": Specify the name of the BUSCO lineage to be used.
-            #"-o": OUTPUT
+            #"-o": defines the folder that will contain all results, logs, and intermediate data
             #"-m": Specify which BUSCO analysis mode to run, genome, proteins, transcriptome (!!!FOR THE CANU AND FLYE ASSEMBLY I USE GENOME AND FOR THE TRINITRY ASSEMBLY I USE TRANSCRIPTOME!!!)
             #"--cpu": Specify the number (N=integer) of threads/cores to use.
 
-    mv ${course_dir}/scripts/${assembly_name} ${assembly_BUSCO_dir}
+#Remove the augustus config directory again
+    rm -r ./augustus_config
