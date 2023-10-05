d390b2eef13031cb9316294318f0e45da9ca5f64
smira_student_account <sabrina.rasch@student.unibe.ch>
2023-10-05 17:06:06 +0200

    update meryl and merqury script

---
+++ 03_05_meryl_evaluation.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=02:00:00
+#SBATCH --job-name=meryl_evaluation
+#SBATCH --mail-user=sabrina.rasch@students.unibe.ch
+#SBATCH --mail-type=begin,end,fail
+#SBATCH --output=/data/users/srasch/assembly_course/Output/output_meryl_evaluation_%j.o
+#SBATCH --error=/data/users/srasch/assembly_course/Error/error_meryl_evaluation_%j.e
+#SBATCH --partition=pall
+
+#Add the modules
+    module add UHTS/Assembler/canu/2.1.1
+
+#Specify directory structure and create them
+    course_dir=/data/users/srasch/assembly_course
+        polish_evaluation_dir=${course_dir}/03_polish_evaluation
+            evaulation_dir=${polish_evaluation_dir}/evaluation
+                meryl_dir=${evaulation_dir}/meryl
+    
+    mkdir ${meryl_dir}
+
+    data_dir=/data/courses/assembly-annotation-course/raw_data/C24/participant_2/Illumina
+
+meryl k=19 count output $SCRATCH/read_1.meryl ${data_dir}/*1.fastq.gz
+
+meryl k=19 count output $SCRATCH/read_2.meryl ${data_dir}/*2.fastq.gz
+
+meryl union-sum output ${meryl_dir}/genome.meryl $SCRATCH/read*.meryl
--- 03_06_merqury_evaluation.sh
+++ 03_06_merqury_evaluation.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=48G
-#SBATCH --time=06:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=02:00:00
 #SBATCH --job-name=merqury_evaluation
 #SBATCH --mail-user=sabrina.rasch@students.unibe.ch
 #SBATCH --mail-type=begin,end,fail
 #SBATCH --output=/data/users/srasch/assembly_course/Output/output_merqury_evaluation_%j.o
 #SBATCH --error=/data/users/srasch/assembly_course/Error/error_merqury_evaluation_%j.e
 #SBATCH --partition=pall
-
-#Add the modules
-    module add UHTS/Quality_control/quast/4.6.0
 
 #Specify name of assembly (!!!COMMENT OUT THE ONE YOU ARE NOT USING!!!)
     assembly_name=canu
@@ -22,6 +19,7 @@
         raw_data_dir=${course_dir}/RawData
         polish_evaluation_dir=${course_dir}/03_polish_evaluation
             evaulation_dir=${polish_evaluation_dir}/evaluation
+                meryl_dir=${evaulation_dir}/meryl
                 merqury_dir=${evaulation_dir}/merqury
                     assembly_merqury_dir=${merqury_dir}/${assembly_name}
     
@@ -29,41 +27,13 @@
     mkdir ${assembly_merqury_dir}
 
 #Specify the assembly to use (!!!COMMENT OUT THE ONE YOU ARE NOT USING!!!)
-    assembly=${polish_evaluation_dir}/polish/pilon/canu/***
-    # assembly=${polish_evaluation_dir}/polish/pilon/flye/***
-
-
-
-#meryl mit raw ilumina reads db vorbereiten
-#meryl in ?canu/2.1.1/bin/meryl?
-#In container bind also ordner of raw data (soft link)
-
-
-
-
-
-
-
-WORKDIR=/path/to/work/directory
+    assembly=${polish_evaluation_dir}/polish/pilon/canu/canu.fasta
+    # assembly=${polish_evaluation_dir}/polish/pilon/flye/flye.fasta
 
 apptainer exec \
---bind $WORKDIR,$RAWDATADIR \
-/software/singularity/containers/Merqury-1.3-1.ubuntu20.sif \
-meryl k=19 count output read_1.meryl read_1.fastq.gz; \
-meryl k=19 count output read_2.meryl read_2.fastq.gz; \
-meryl union-sum output genome.meryl read*.meryl
-
-#read_1.meryl in $SCRATCH
-#read_1.fast.gz -> raw ilumina read
-#genome.meryl in folder -> full path
-
-
-apptainer exec \
---bind $WORKDIR \
+--bind $course_dir \
 /software/singularity/containers/Merqury-1.3-1.ubuntu20.sif \
 merqury.sh \
-genome.meryl \ #FULLPATH TO MERYL CREATED ABOVE
-assemblies.fasta \ #FULLPATH TO ASSEMBLY THAT SHOULD BE EVALUATED
-flye_test #FULLPATH TO OUTPUT PREFIX
-
-#MAKE TWO SCRIPTS ONE WITH MERYL AND ONE WITH MERCURY
+${meryl_dir}/genome.meryl \ 
+${assembly} \ 
+${assembly_merqury_dir}/${assembly_name}
