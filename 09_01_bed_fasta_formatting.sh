3093e7c3dc89da30614d88979f0659ae3921625d
smira_student_account <sabrina.rasch@student.unibe.ch>
2023-11-22 10:49:27 +0100

    change directory structure and add copying of files

--- 09_01_bed_fasta_formatting.sh
+++ 09_01_bed_fasta_formatting.sh
@@ -18,13 +18,17 @@
 #Specify directory structure and create them
     course_dir=/data/users/srasch/assembly_course
         input_dir=${course_dir}/07_prot_annotation/polished.maker.output.renamed
-        genespace_dir=${course_dir}/09_genespace
-            bed_dir=${genespace_dir}/bed
-            peptide_dir=${genespace_dir}/peptide
+        comp_genomics_dir=${course_dir}/09_comp_genomics
+            bed_dir=${comp_genomics_dir}/bed
+            peptide_dir=${comp_genomics_dir}/peptide
+            noseq_dir=${comp_genomics_dir}/noseq
+            proteins_dir=${comp_genomics_dir}/proteins
 
-    mkdir ${genespace_dir}
+    mkdir ${comp_genomics_dir}
     mkdir ${bed_dir}
     mkdir ${peptide_dir}
+    mkdir ${noseq_dir}
+    mkdir ${proteins_dir}
 
 #Specify the assembly to use
     genome="polished"
@@ -33,16 +37,38 @@
     input_proteins=${input_dir}/${genome}.all.maker.proteins.fasta.renamed.fasta
 
 #Get all contings and sort them by size
-    awk '$3=="contig"' ${input_noseq} | sort -t $'\t' -k5 -n -r > ${genespace_dir}/${data}_size_sorted_contigs.txt
+    awk '$3=="contig"' ${input_noseq} | sort -t $'\t' -k5 -n -r > ${comp_genomics_dir}/${data}_size_sorted_contigs.txt
 
 #Get the 10 longest
-    head -n10 ${genespace_dir}/${data}_size_sorted_contigs.txt | cut -f1 > ${genespace_dir}/${data}_contigs.txt
+    head -n10 ${comp_genomics_dir}/${data}_size_sorted_contigs.txt | cut -f1 > ${comp_genomics_dir}/${data}_contigs.txt
 
 #Create bed file
-    awk '$3=="mRNA"' ${input_noseq} | cut -f 1,4,5,9 | sed 's/ID=//' | sed 's/;.\+//' | grep -w -f ${genespace_dir}/${data}_contigs.txt > ${bed_dir}/${data}.bed
+    awk '$3=="mRNA"' ${input_noseq} | cut -f 1,4,5,9 | sed 's/ID=//' | sed 's/;.\+//' | grep -w -f ${comp_genomics_dir}/${data}_contigs.txt > ${bed_dir}/${data}.bed
 
 #Get the gene IDs
-    cut -f4 ${bed_dir}/${data}.bed > ${genespace_dir}/${data}_gene_IDs.txt
+    cut -f4 ${bed_dir}/${data}.bed > ${comp_genomics_dir}/${data}_gene_IDs.txt
 
 #Create fasta file
-    cat ${input_proteins} | seqkit grep -r -f ${genespace_dir}/${data}_gene_IDs.txt | seqkit seq -i > ${peptide_dir}/${data}.fa
+    cat ${input_proteins} | seqkit grep -r -f ${comp_genomics_dir}/${data}_gene_IDs.txt | seqkit seq -i > ${peptide_dir}/${data}.fa
+
+#Copy files and change permissions (could run this directly in the terminal)
+    # CDS_Genespace_dir=/data/courses/assembly-annotation-course/CDS_annotation/Genespace
+    # CDS_busco_dir=/data/courses/assembly-annotation-course/CDS_annotation/busco
+    # CDS_TE_dir=/data/courses/assembly-annotation-course/CDS_annotation/TE
+
+    # cp ${input_noseq} ${CDS_Genespace_dir}/${data}.all.maker.noseq.gff.renamed.gff
+    # cp ${input_proteins} ${CDS_Genespace_dir}/${data}.all.maker.proteins.fasta.renamed.fasta
+    # cp ${bed_dir}/${data}.bed ${CDS_Genespace_dir}/${data}.bed
+    # cp ${peptide_dir}/${data}.fa ${CDS_Genespace_dir}/${data}.fa
+    # cp ${course_dir}/03_polish_evaluation/evaluation/BUSCO/flye/flye/short_summary.specific.brassicales_odb10.flye.txt ${CDS_busco_dir}/short_summary.specific.brassicales_odb10.${data}_genome.txt
+    # cp ${course_dir}/08_Quality/BUSCO/BUSCO/short_summary.specific.brassicales_odb10.BUSCO.txt ${CDS_busco_dir}/short_summary.specific.brassicales_odb10.${data}_proteins.txt
+    # cp ${course_dir}/05_TE_annotation/TE_annotator/polished.fasta.mod.EDTA.TEanno.gff3 ${CDS_TE_dir}/${data}.mod.EDTA.TEanno.gff3
+    # cp ${course_dir}/05_TE_annotation/TE_annotator/polished.fasta.mod.EDTA.TEanno.sum ${CDS_TE_dir}/${data}.mod.EDTA.TEanno.sum
+
+    # chmod +r ${CDS_Genespace_dir}/${data}.all.maker.noseq.gff.renamed.gff ${CDS_Genespace_dir}/${data}.all.maker.proteins.fasta.renamed.fasta ${CDS_Genespace_dir}/${data}.bed ${CDS_Genespace_dir}/${data}.fa ${CDS_busco_dir}/short_summary.specific.brassicales_odb10.${data}_genome.txt ${CDS_busco_dir}/short_summary.specific.brassicales_odb10.${data}_proteins.txt ${CDS_TE_dir}/${data}.mod.EDTA.TEanno.gff3 ${CDS_TE_dir}/${data}.mod.EDTA.TEanno.sum
+
+    # cp ${CDS_Genespace_dir}/*.bed ${bed_dir}
+    # cp ${CDS_Genespace_dir}/*.fa ${peptide_dir}
+    # cp ${CDS_Genespace_dir}/*.gff ${noseq_dir}
+    # cp ${CDS_Genespace_dir}/*.fasta ${proteins_dir}
+    
